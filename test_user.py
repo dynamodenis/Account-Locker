8d172e75af7117f49f538ddff22ee7f7508d76ca
Dynamo Denis <dmbugua66@gmail.com>
2020-04-17 12:40:51 +0300

    created a test method that tests the user class _init_ method

--- test_user.py
+++ test_user.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3.6
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+         '''
+        self.user=User("Denis","Mbugua","093099308","dmbugua66@gmail.com",'Slack Account')
+
+    def test_init_(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.user.first_name,"Denis")
+        self.assertEqual(self.user.last_name,"Mbugua")
+        self.assertEqual(self.user.password,"093099308")
+        self.assertEqual(self.user.email,"dmbugua66@gmail.com")
+        self.assertEqual(self.user.description,"Slack Account")
+
+
+if __name__=="__main__":
+    unittest.main()
--- user.py
+++ user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3.6
 class User:
+    userList=[]
     def __init__(self,first_name,last_name,password,email,description):
        self.first_name=first_name
        self.last_name=last_name
        self.password=password
        self.email=email
        self.description=description
-new=User("Denis","Mbgua","093099308","dmbugua66@gmail.com",'Slack Account')
-print(new.email) 
+user=User("Denis","Mbgua","093099308","dmbugua66@gmail.com",'Slack Account')
