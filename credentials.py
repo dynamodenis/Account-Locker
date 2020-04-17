4b5f43a7bceee7588628753521e8765ec0b88d8d
Dynamo Denis <dmbugua66@gmail.com>
2020-04-17 11:44:58 +0300

    created a credentials class

---
+++ credentials.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+class Credentials:
+
+    def __init__(self,username,password,details):
+        self.username=username
+        self.password=password
+        self.details=details
+
+new=Credentials("dynamodenis","987654321","hello world")
